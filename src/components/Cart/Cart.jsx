97bd7404b75f6c71616ac735438218ee5182d28a
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-03-28 15:40:41 +0600

    done

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,9 +7,12 @@ const Cart = ({cart}) => {
     // calculate
     let total = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for (const product of cart ){
-        total = total + product.price;
+        product.quantity = product.quantity || 1;
+        total = total + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping
+        quantity = quantity + product.quantity;
     }
     const tax = total *0.07;
     const grandTotal = total + totalShipping + tax
@@ -17,7 +20,7 @@ const Cart = ({cart}) => {
     return (
         <div className='cart'>
                 <h4>Order Summery</h4>
-                <p>Selected item {cart.length}</p>
+                <p>Selected item {quantity}</p>
                 <p>Total Price: ${total} </p>
                 <p>Total Shipping: ${totalShipping}</p>
                 <p>Tax ${tax.toFixed(2)}</p>
